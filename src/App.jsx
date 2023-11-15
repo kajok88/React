ebab15147d639cbe4cb866da1bd8bd7dfb64cbf4
kajok88 <skyborgg88@gmail.com>
2023-11-15 11:34:33 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,209 +1,74 @@
-// import React, { useState } from 'react';
-
 import React, { useState, useEffect } from 'react';
-import personService from './services/persons'
-import Filter from './components/Filter';
-import PersonForm from './components/PersonForm';
-import Persons from './components/Person';
-import Notification from './components/Notification'
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
-  const [message, setMessage] = useState(null);
-
+  const [searchTerm, setSearchTerm] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
-    personService
-      .getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons);
-        console.log('promise fulfilled')
-        setMessage(null)
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setCountries(response.data);
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
-        setMessage(
-          `Json server could not be accessed.`
-        );
-        setTimeout(() => {
-          getAll();
-        }, 3000);
+        console.error('Error fetching countries:', error);
       });
-      
   }, []);
 
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    // Tarkistetaan, ettei nimi tai numero ole tyhjiä
-    if (newName.trim() === '' || newNumber.trim() === '') {
-      alert('Give a name and a number.');
+  useEffect(() => {
+    if (searchTerm.trim() === '') {
+      setFilteredCountries([]);
       return;
     }
 
-    const existingPerson = persons.find(person => person.name === newName);
-    if (existingPerson) {
-      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)){
-        // handleUpdate(existing.id);
-        personService
-          .update(existingPerson.id, {...existingPerson, number: newNumber})
-          .then(response => {
-            setPersons(persons.map(person => (person.id === existingPerson.id ? response : person)));
-            setNewName('');
-            setNewNumber('');
-            setMessage(
-              `${existingPerson.name} was successfully updated`
-            );
-            setTimeout(() => {
-              setMessage(null)
-            }, 2500);
-          })
-          .catch(error => {
-            console.error('Error updating person:', error);
-            setPersons(persons.filter(person => person.id !== id));
-            setNewName('');
-            setNewNumber('');
-            setMessage(
-              `${existingPerson.name} was already deleted from server`
-            );
-          });
-      }
+    const filtered = countries.filter(country =>
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (filtered.length > 10) {
+      setFilteredCountries([]);
     } else {
-      const newPerson = {
-        name: newName,
-        number: newNumber,
-      };
-      // 2.12 -> Lähetetään POST-pyyntö palvelimelle uuden henkilön lisäämiseksi
-      personService
-        .create(newPerson)
-        .then(returnedPerson => {
-          setPersons([...persons, returnedPerson]); // Päivitetään myös paikallinen tila
-          setNewName('');
-          setNewNumber('');
-          setMessage(
-            `${newName} was successfully added`
-          );
-          setTimeout(() => {
-            setMessage(null)
-          }, 2500);
-        })
-        .catch(error => {
-          console.error('Error adding a person:', error);
-          setPersons(persons.filter(person => person.id !== returnedPerson));
-            setNewName('');
-            setNewNumber('');
-            setMessage(
-              `Json server could not be accessed.`
-            );
-            setTimeout(() => {
-              setMessage(null)
-            }, 2500);
-        });
-    };
-  };
-
-  const handleUpdate = (id) => {
-    const personToUpdate = persons.find(person => person.id === id);
-    const name = personToUpdate.name;
-    const number = window.prompt(`Update number for ${name}`, personToUpdate.number);
-
-    if (number !== null) {
-      personService
-        .update(id, {...personToUpdate, number: number})
-        .then(response => {
-          setPersons(persons.map(person => (person.id === id ? response : person)));
-          setMessage(
-            `${personToUpdate.name} was successfully updated`
-          );
-          setTimeout(() => {
-            setMessage(null)
-          }, 2500);
-        })
-        .catch(error => {
-          console.error('Error updating person:', error);
-          setPersons(persons.filter(person => person.id !== id));
-          setNewName('');
-          setNewNumber('');
-          setMessage(
-            `Information of ${personToUpdate.name} was already deleted from server`
-          );
-          setTimeout(() => {
-            setMessage(null)
-          }, 2500);
-        });
+      setFilteredCountries(filtered);
     }
-  };
+  }, [searchTerm, countries]);
 
-  const handleDelete = (id, name) => {
-    if (window.confirm(`Delete ${name}?`)) {
-      personService
-        .remove(id)
-        .then(() => {
-          setPersons(persons.filter(person => person.id !== id));
-          setMessage(
-            `${name} was successfully deleted`
-          );
-          setTimeout(() => {
-            setMessage(null)
-          }, 2500);
-        })
-        .catch(error => {
-          console.error('Error deleting a person:', error);
-        });
-    }
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredPersons = persons.filter(person =>
-    person.name.toLowerCase().includes(filter.toLowerCase())
-  );
-  
 
   return (
     <div>
-      
-      <h2>Countries</h2>
-
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <Notification message={message} />
-
-      <h3>Add a new</h3>
-
-      <PersonForm
-        newName={newName}
-        newNumber={newNumber}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
-        addPerson={addPerson}
-      />
-
-      <h3>Numbers</h3>
-
-      <Persons
-        persons={filteredPersons}
-        handleDelete={handleDelete}
-        handleUpdate={handleUpdate}
-      />
+      <h2>Country Information</h2>
+      <form>
+        <label htmlFor="search">Search countries:</label>
+        <input
+          type="text"
+          id="search"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </form>
+      {filteredCountries.length === 0 && searchTerm.trim() !== '' && (
+        <p>No matching countries found. Please refine your search.</p>
+      )}
+      {filteredCountries.length > 10 && (
+        <p>Too many matches, please specify your search.</p>
+      )}
+      {filteredCountries.length <= 10 && (
+        <ul>
+          {filteredCountries.map(country => (
+            <li key={country.alpha2Code}>
+              <h3>{country.name.common}</h3>
+              <p>Capital: {country.capital}</p>
+              <p>Population: {country.population}</p>
+              <img src={country.flags.svg} alt={`Flag of ${country.name.common}`} style={{ width: '100px' }} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-
-
-
-
-export default App
+export default App;
--- src/App2.jsx
+++ src/App2.jsx
@@ -1,35 +1,209 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useState, useEffect } from 'react';
+import personService from './services/persons'
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Person';
+import Notification from './components/Notification'
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [message, setMessage] = useState(null);
+
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons);
+        console.log('promise fulfilled')
+        setMessage(null)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setMessage(
+          `Json server could not be accessed.`
+        );
+        setTimeout(() => {
+          getAll();
+        }, 3000);
+      });
+      
+  }, []);
+
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    // Tarkistetaan, ettei nimi tai numero ole tyhjiä
+    if (newName.trim() === '' || newNumber.trim() === '') {
+      alert('Give a name and a number.');
+      return;
+    }
+
+    const existingPerson = persons.find(person => person.name === newName);
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)){
+        // handleUpdate(existing.id);
+        personService
+          .update(existingPerson.id, {...existingPerson, number: newNumber})
+          .then(response => {
+            setPersons(persons.map(person => (person.id === existingPerson.id ? response : person)));
+            setNewName('');
+            setNewNumber('');
+            setMessage(
+              `${existingPerson.name} was successfully updated`
+            );
+            setTimeout(() => {
+              setMessage(null)
+            }, 2500);
+          })
+          .catch(error => {
+            console.error('Error updating person:', error);
+            setPersons(persons.filter(person => person.id !== id));
+            setNewName('');
+            setNewNumber('');
+            setMessage(
+              `${existingPerson.name} was already deleted from server`
+            );
+          });
+      }
+    } else {
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+      };
+      // 2.12 -> Lähetetään POST-pyyntö palvelimelle uuden henkilön lisäämiseksi
+      personService
+        .create(newPerson)
+        .then(returnedPerson => {
+          setPersons([...persons, returnedPerson]); // Päivitetään myös paikallinen tila
+          setNewName('');
+          setNewNumber('');
+          setMessage(
+            `${newName} was successfully added`
+          );
+          setTimeout(() => {
+            setMessage(null)
+          }, 2500);
+        })
+        .catch(error => {
+          console.error('Error adding a person:', error);
+          setPersons(persons.filter(person => person.id !== returnedPerson));
+            setNewName('');
+            setNewNumber('');
+            setMessage(
+              `Json server could not be accessed.`
+            );
+            setTimeout(() => {
+              setMessage(null)
+            }, 2500);
+        });
+    };
+  };
+
+  const handleUpdate = (id) => {
+    const personToUpdate = persons.find(person => person.id === id);
+    const name = personToUpdate.name;
+    const number = window.prompt(`Update number for ${name}`, personToUpdate.number);
+
+    if (number !== null) {
+      personService
+        .update(id, {...personToUpdate, number: number})
+        .then(response => {
+          setPersons(persons.map(person => (person.id === id ? response : person)));
+          setMessage(
+            `${personToUpdate.name} was successfully updated`
+          );
+          setTimeout(() => {
+            setMessage(null)
+          }, 2500);
+        })
+        .catch(error => {
+          console.error('Error updating person:', error);
+          setPersons(persons.filter(person => person.id !== id));
+          setNewName('');
+          setNewNumber('');
+          setMessage(
+            `Information of ${personToUpdate.name} was already deleted from server`
+          );
+          setTimeout(() => {
+            setMessage(null)
+          }, 2500);
+        });
+    }
+  };
+
+  const handleDelete = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id));
+          setMessage(
+            `${name} was successfully deleted`
+          );
+          setTimeout(() => {
+            setMessage(null)
+          }, 2500);
+        })
+        .catch(error => {
+          console.error('Error deleting a person:', error);
+        });
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      
+      <h2>Countries</h2>
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <Notification message={message} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons
+        persons={filteredPersons}
+        handleDelete={handleDelete}
+        handleUpdate={handleUpdate}
+      />
+    </div>
+  );
+};
+
+
+
+
 
 export default App
