b67348c0040840bb85eeb82424a84da7fcf21864
kajok88 <skyborgg88@gmail.com>
2024-02-06 18:12:09 +0200

    Search UI updated

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,4 @@
-
+import { Button, Card } from "react-bootstrap";
 import "./Countries.css"
 
 const Countries = ({ countriesToShow, setCountriesToShow }) => {
@@ -10,17 +10,21 @@ const Countries = ({ countriesToShow, setCountriesToShow }) => {
     return (
       <>
         <div>
-          {countriesToShow.length > 100 ? (
+          {countriesToShow.length > 20 ? (
             null
           ) : (
-            countriesToShow.map((country) => (
-              <div key={country.name.official} className="search-result">
-                {country.name.common}{" "}
-                <button onClick={() => setCountriesToShow([country])}>
-                  Show
-                </button>
+
+              <div className="d-flex flex-column">
+                {countriesToShow.map((country) => (
+                  <Card key={country.name.official} style={{ borderRadius: '0' }} className="search-result">
+                    <Card.Body className="d-flex justify-content-between align-items-center">
+                      <span>{country.name.common}</span>
+                      <Button variant="success" onClick={() => setCountriesToShow([country])}>Show</Button>
+                    </Card.Body>
+                  </Card>
+                ))}
               </div>
-            ))
+
           )}
         </div>
       </>
