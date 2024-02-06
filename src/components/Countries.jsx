2c9a18b81693b55e3f3854963b76e0d214a9f6f3
kajok88 <skyborgg88@gmail.com>
2024-02-06 18:41:40 +0200

    Hide search list after clicking the button

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import { Button, Card } from "react-bootstrap";
 import "./Countries.css"
 
 const Countries = ({ countriesToShow, setCountriesToShow, setSelectedCountry, setQuery }) => {
 
-    // if (countriesToShow.length === 1)   {
-    //   return null;
-    // }
+  const [visible, setVisible] = useState(true);
+
+  const handleOnClick = (country) => {
+    setCountriesToShow([country]);
+    setSelectedCountry([country]);
+    setQuery("");
+    setVisible(false);
+  };
 
     return (
       <>
@@ -15,11 +21,14 @@ const Countries = ({ countriesToShow, setCountriesToShow, setSelectedCountry, se
           ) : (
               <div className="d-flex flex-column">
                 {countriesToShow.map((country) => (
-                  <Card key={country.name.official} style={{ borderRadius: '0' }} className="search-result">
+                  <Card key={country.name.official} 
+                    style={{ borderRadius: '0', display: visible ? 'block' : 'none' }} 
+                    className="search-result">
+                      
                     <Card.Body className="d-flex justify-content-between align-items-center">
                       <span>{country.name.common}</span>
                       <Button variant="success" 
-                        onClick={() => {setCountriesToShow([country]), setSelectedCountry([country]), setQuery("")}}>
+                        onClick={() => handleOnClick(country)}>
                           Show
                       </Button>
                     </Card.Body>
