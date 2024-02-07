be607c80925c10c0eefe1d945aece8fa2984a943
kajok88 <skyborgg88@gmail.com>
2024-02-07 23:39:05 +0200

    Added error hangling for fetching country data

--- src/components/CountryForm.jsx
+++ src/components/CountryForm.jsx
@@ -8,9 +8,13 @@ const CountryForm = ({ country }) => {
       <div>Area: {country.area} km²</div>
       <h3>Languages:</h3>
       <ul>
-        {Object.values(country.languages).map((language) => (
-          <li key={language}>{language}</li>
-        ))}
+        {country.languages ? (
+          Object.values(country.languages).map((language, index) => (
+            <li key={index}>{language}</li>
+          ))
+        ) : (
+          <p>No known languages</p>
+        )}
       </ul>
       <img src={country.flags.png} alt={`${country.name.common} flag`} />
       {/* <WeatherInfo city={country.capital} lat="61" lng="24" /> */}
