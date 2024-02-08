9f230a9856b8bfb1c5642451f6ee4a0e9997c7ad
kajok88 <skyborgg88@gmail.com>
2024-02-08 22:26:37 +0200

    Cleaned up some code

--- src/App.jsx
+++ src/App.jsx
@@ -36,25 +36,9 @@ import { useCountryData } from "./contexts/CountryDataContext";
 
 
 const App = () => {
-  // For Searching and showing those countries
   const [query, setQuery] = useState("");
-  // const [countries, setCountries] = useState([]);
   const countries = useCountryData();
   const [countriesToShow, setCountriesToShow] = useState([]);
-  // const [showApiSubmitPage, setShowApiSubmitPage] = useState(false);
-
-  const [countryCoordinates, setCountryCoordinates] = useState(null);
-  const [capitalCoordinates, setCapitalCoordinates] = useState(null);
-
-  const [selectedCountry, setSelectedCountry] = useState("");
-  const [selectedCity, setSelectedCity] = useState("");
-
-
-  // useEffect(() => {
-  //   axios.get("https://restcountries.com/v3.1/all").then((response) => {
-  //     setCountries(response.data);
-  //   });
-  // }, []);
 
 
   const handleQueryChange = (event) => {
@@ -68,27 +52,17 @@ const App = () => {
     );
   };
 
-  const handleSearchChange = (e) => {
-    setSearchValue(e.target.value);
-    // Show the card when there's some input
-    setShowCard(!!e.target.value);
-  };
-
-  const GetCountryCoodrinates2 = () => {
-    if (countriesToShow.length === 1 )
-    return (
-      <p>{countriesToShow[0].region}</p>
-    );
+  const handleClose = () => {
+    setCountriesToShow([]); // This essentially resets the component
   };
   
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark" sticky="top">
         <Container>
-          <Navbar.Brand href="#home" >
+          <Navbar.Brand href="#home">
             <img
-              alt=""
-              // src="/img/logo.svg"
+              alt="logo.png"
               src="./map_point.png"
               width="30"
               height="30"
@@ -130,78 +104,50 @@ const App = () => {
       </Navbar>
         
       <div>
-        {/* {showApiSubmitPage ? (
-          <>
-
-              <SubmitApiKeys setShowApiSubmitPage={setShowApiSubmitPage} onSubmit={handleHideSubmitPage} />
-
-          </>
-        ) : ( */}
-          <div>
-          
-            {/* <button onClick={handleShowSubmitPage}>Modify API keys</button><br></br>
-            {/* {showApiSubmitPage ? (
-              <SubmitApiKeys setShowApiSubmitPage={setShowApiSubmitPage} onSubmit={handleHideSubmitPage}/>
-            ) : null} */}
-            {/* <ApiSubmitPage showPage={ApiSubmitPage}></ApiSubmitPage> */}
-            
-            {/* <div>
-              Find countries <input value={query} onChange={handleQueryChange} />
-            </div> */}
-            <div>
-              
-              <Container className="">
-                <Row className="justify-content-center">
-                  <Col xs={12} md={5}>
-                    <div className="floating-search-card">
-                    {query ? (
-                      <Countries
-                      countriesToShow={countriesToShow}
-                      setCountriesToShow={setCountriesToShow}
-                      setSelectedCountry={setSelectedCountry}
-                      setQuery={setQuery}
-                    />
-                    ) : null}
-                      
-                    </div>
-                  </Col>
-                </Row>
-              </Container>           
-            </div>
-            
-            <div>
-              {countriesToShow.length === 1 ? (
-                <Container className="">
-                  <Row className="justify-content-center align-items-center"> {/* Added align-items-center */}
-                    <Col xs={12} md={2}>
-                      <div className="floating-info-card with-blue-border">
-                        <CountryForm country={countriesToShow[0]} />
-                      </div>
-                    </Col>
-                  </Row>
-                </Container>
-              ) : null}
-            </div>
-            <div>
-              {/* <Leaflet/> */}
-              {countriesToShow.length === 1 ? (
-                <>
-                  <GetCountryCoodrinates country={countriesToShow[0]}/>
-                </>
-              ) : (
-                <>
-                  <GetCountryCoodrinates />
-                </>)}
-              {/* <GetCountryCoodrinates></GetCountryCoodrinates>
-              <Map /> */}
-              {/* <GetcountryCoodrinates ></GetcountryCoodrinates> */}
-            </div>
-            
-          </div>
-        {/* )} */}
+        <div>
+          <Container className="">
+            <Row className="justify-content-center">
+              <Col xs={12} md={5}>
+                <div className="floating-search-card">
+                {query ? (
+                  <Countries
+                  countriesToShow={countriesToShow}
+                  setCountriesToShow={setCountriesToShow}
+                  setQuery={setQuery}
+                />
+                ) : null}
+                  
+                </div>
+              </Col>
+            </Row>
+          </Container>           
+        </div>
+        
+        <div>
+          {countriesToShow.length === 1 ? (
+            <Container className="">
+              <Row className="justify-content-center align-items-center"> {/* Added align-items-center */}
+                <Col xs={12} md={2}>
+                  <div className="floating-info-card with-blue-border">
+                    <CountryForm country={countriesToShow[0]} handleClose={handleClose} />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          ) : null}
+        </div>
+        <div>
+          {countriesToShow.length === 1 ? (
+            <>
+              <GetCountryCoodrinates country={countriesToShow[0]}/>
+            </>
+          ) : (
+            <>
+              <GetCountryCoodrinates />
+            </>)}
+        </div>
       </div>
     </>
-    
   );
 };
 
